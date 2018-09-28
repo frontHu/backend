b07ccc6a5b69c63af38573943f93fe3515237cb7
hzw <405163706@qq.com>
2018-09-28 11:08:39 +0800

    d

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,11 @@ html, body {
   width: 100%;
   height: 100%;
   color: #606c71;
-<<<<<<< HEAD
   padding: 0;
   margin: 0;
-=======
-  margin: 0;
-  padding: 0;
->>>>>>> d16215af17cfb3a79a7a3966247330113288389c
 }
 #app {
   width: 100%;
   height: 100%;
 }
-</style>
+</style> 
